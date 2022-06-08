b14034855fff2bd09844d1f9edb1aaec7d3516be
pierrelouisIV <56440522+pierrelouisIV@users.noreply.github.com>
2022-06-08 12:40:26 +0200

    supra diem

---
+++ Primi_numeri/primalite/main.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+#define BOOL  unsigned int
+#define TRUE  1
+#define FALSE 0
+
+/*  compilation :  gcc -o prog main.c -lm          */
+
+//
+clock_t start, end;
+
+//
+typedef unsigned long long u64;
+
+
+// Exhaustive algorithm :
+BOOL classical_isprime(u64 n)
+{
+    //
+    for(u64 i = 2; i < n; ++i)
+    {
+        if (n%i == 0)
+            return FALSE;
+    }
+
+    //
+    return TRUE;
+}
+
+// much faster than the first algo
+BOOL second_isprime(u64 n)
+{
+    //
+    for(u64 d = 2; pow(d,2) < (n+1); ++d)
+    {
+        //
+        if (n%d == 0)
+            return FALSE;
+    }
+
+    //
+    return TRUE;
+}
+
+
+// Create a prime number list
+u64 *crible_eratosthene(u64 n)
+{
+    // var
+    u64 index = 0;
+    // allocate mem
+    u64 * L = malloc(sizeof(u64) * n);
+
+    // fill the list
+    for(u64 i = 2; i < n; ++i)
+    {
+        if (second_isprime(i))
+        {
+            L[index] = i;
+            index++;
+        }
+    }
+    //
+    return L;
+}
+
+
+
+
+//
+int main(int argc, char **argv)
+{
+
+    // test if a number is prime
+    if (argc == 2)
+    {
+        //
+        u64 integer = atoi(argv[1]);
+        if (second_isprime(integer))
+            printf("%llu is prime \n", integer);
+        else
+            printf("%llu is not prime \n", integer);
+    }
+    else
+    {
+        printf("Usage: %s [integer] \n", argv[0]);
+    }
+
+    //
+    return 0;
+}
